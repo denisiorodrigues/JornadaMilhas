e58816b142e373151991cd0a147917e819c4e520
Denisio Rordrigues <denisio@ymail.com>
2025-01-10 16:39:33 -0300

    updadte musica teste

--- test/JornadaMilhas.Test/MusicaTest.cs
+++ test/JornadaMilhas.Test/MusicaTest.cs
@@ -4,26 +4,29 @@ namespace JornadaMilhas.Test;
 
 public class MusicaTest
 {
-    [Fact]
-    public void ValidarNomeDaMusica()
+    [Theory]
+    [InlineData("")]
+    [InlineData("Minha História")]
+    public void InicializaNomeCorretamenteQuandoCastradaNovaMusica(string nome)
     {
         //Arrange
-        string nomeDaMusica = "JornadaMilhas.Test";
         
         //Act
-        var musica = new Musica(nomeDaMusica);
+        var musica = new Musica(nome);
         
         //Assert
         Assert.NotNull(musica);
-        Assert.Equal(nomeDaMusica, musica.Nome);
+        Assert.Equal(nome, musica.Nome);
     }
     
-    [Fact]
-    public void ValidaroIndetificaroDaMusicaFoiinicializado()
+    [Theory]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(0)]
+    public void ValidaroSeOIndetificadorDaMusicaFoiInicializado(int id)
     {
         //Arrange
         string nomeDaMusica = "JornadaMilhas.Test";
-        int id = 1;
 
         //Act
         var musica = new Musica(nomeDaMusica) { Id = id };
@@ -33,20 +36,40 @@ public class MusicaTest
         Assert.Equal(id, musica.Id);
     } 
     
-    [Fact]
-    public void ValidaroRetornoToStringDeUmObjetoMusica()
+    [Theory]
+    [InlineData("Música Teste", "Nome: Música Teste")]
+    [InlineData("Outra Música", "Nome: Outra Música")]
+    [InlineData("Mais uma Música", "Nome: Mais uma Música")]
+    public void ExibeDadosDaMusicaCorretamenteQuandoChamadoMetodoToString
+        (string nome, string saidaEsperada)
+    {
+        // Arrange
+        Musica musica = new Musica(nome);
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        // Act
+        musica.ExibirFichaTecnica();
+        string saidaAtual = stringWriter.ToString().Trim();
+
+        // Assert
+        Assert.Equal(saidaEsperada, saidaAtual);
+    }
+    
+    [Theory]
+    [InlineData(1, "Música Teste", "Id: 1 Nome: Música Teste")]
+    [InlineData(2, "Outra Música", "Id: 2 Nome: Outra Música")]
+    [InlineData(3, "Mais uma Música", "Id: 3 Nome: Mais uma Música")]
+    public void RetornarToStringDoObjetoEValidandoORsultadoEsperado(int id, string nome, string toStringEsperado)
     {
         //Arrange
-        string nomeDaMusica = "JornadaMilhas.Test";
-        int id = 1;
-        var musica = new Musica(nomeDaMusica) { Id = id };
-        string resultadoEsperado = $"Id: {id} Nome: {nomeDaMusica}";
+        var musica = new Musica(nome) { Id = id };
         
         //Act
         string resultado = musica.ToString();
         
         //Assert
         Assert.NotNull(musica);
-        Assert.Equal(resultado, resultadoEsperado);
+        Assert.Equal(resultado, toStringEsperado);
     } 
 }
