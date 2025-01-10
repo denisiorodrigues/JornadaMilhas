151a1edad14d66b54102cd5376d011935ba0d940
Denisio Rordrigues <denisio@ymail.com>
2025-01-10 11:10:10 -0300

    teste: oferta com rota nula

--- test/JornadaMilhas.Test/OfertaViagemTest.cs
+++ test/JornadaMilhas.Test/OfertaViagemTest.cs
@@ -16,4 +16,18 @@ public class OfertaViagemTest
         
         Assert.Equal(validacao, oferta.EhValido);
     }
+    
+    [Fact]
+    public void TestandoOfertaComRotaNula()
+    {
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2025, 01, 08), new DateTime(2025, 02, 02));
+        double preco = 100.00;
+        var validacao = true;
+        
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+        
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
+    }
 }
